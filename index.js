892d793e2c2add93e51c56a051a8e65564fd762a
Рустам Оруджалиев <slu4ai.rw@gmail.com>
2020-12-18 16:46:51 +0300

    add fn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log('Hello, Hexlet\!');
+console.log("Hello, Hexlet!");
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
-
+console.log(_.last(["one", "two"]));
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};

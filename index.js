19897aa8abaec5f4c60119e0111aa577a8bee3c0
Рустам Оруджалиев <slu4ai.rw@gmail.com>
2020-12-19 00:34:15 +0300

    add solution "Sum of square"

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
+const sumSquareDifference = (n) => {
+  // разность квадрата суммы и суммы квадратов последовательности натуральных чисел от 1 до n
+  const square = (m) => m * m;
+  // сумма натуральных чисел от 1 до а
+  const sumSequence = (a) => (a === 1 ? 1 : a + sumSequence(a - 1));
+  // сумма квадратов натуральных чисел
+  const sumSquare = (b) => (b === 1 ? 1 : b * b + sumSquare(b - 1));
+  // квадрат суммы натуральных чисел от 1 до с
+  const squareOfSum = (c) => square(sumSequence(c));
+  if (n < 1) {
+    return NaN;
+  }
+  return squareOfSum(n) - sumSquare(n);
 };
-fn(1, 2);
+sumSquareDifference(22);
